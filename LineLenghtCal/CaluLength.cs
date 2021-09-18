ba2e3f1b565dca757ecf805517cb1c44af026e14
mounika <mounaadagatla@gmail.com>
2021-09-18 07:53:57 +0530

    [Mounika] Uc2 Added

--- LineLenghtCal/CaluLength.cs
+++ LineLenghtCal/CaluLength.cs
@@ -6,5 +6,10 @@ namespace LineLenghtCal
 {
     class CaluLength
     {
+        public double LengthCal(int x1,int x2,int y1,int y2)
+        {
+            double length = Math.Sqrt(Math.Pow(x2 - x1, 2)+Math.Pow(y2-y1,2));
+            return length;
+        }
     }
 }
