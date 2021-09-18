8f9d76228f71e9696c4f4507a19c10530c1f1f92
mounika <mounaadagatla@gmail.com>
2021-09-18 07:38:18 +0530

    [Mounika] Add:main class in LinComaprision

---
+++ LineLenghtCal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineLenghtCal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is Line comparision");
+        }
+    }
+}
